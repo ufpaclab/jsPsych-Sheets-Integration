b69e48d9751593a83ab90340a8928900c3903e5c
GreyJohnsonGit <grey.johnson@ufl.edu>
2020-10-05 01:44:13 -0400

    fixed redirect

--- sona/SONA.js
+++ sona/SONA.js
@@ -1,6 +1,6 @@
 const SONA = {
     redirectToSONA: function() {
         let survey_code = new URLSearchParams(window.location.search).get("survey_code")
-        window.location.href = `https://ufl.sona-systems.com/webstudy_credit.aspx?experiment_id=145&credit_token=9419af749b2c4e79b6a43900be9aec70&survey_code=${survey_code}`
+        window.top.location.href = `https://ufl.sona-systems.com/webstudy_credit.aspx?experiment_id=145&credit_token=9419af749b2c4e79b6a43900be9aec70&survey_code=${survey_code}`
     }
 }

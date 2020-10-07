b86afab3714228297ea695271070186a7a6266e2
Grey Johnson <gjohnson@osisoft.com>
2020-10-07 12:43:36 -0400

    SONA Integration

--- ExampleExperiment.js
+++ ExampleExperiment.js
@@ -1,9 +1,10 @@
-function ExampleExperiment(jsSheetHandle, jsPsychHandle, SONAHandle) {
+function ExampleExperiment(jsSheetHandle, jsPsychHandle, survey_code) {
     jsSheetHandle.CreateSession(RunExperiment)
 
     function RunExperiment(session) {
         // Define Constants
-        const CONTACT_EMAIL = "fake@email.com"
+        const CONTACT_EMAIL = 'fake@email.com'
+        const SONA_URL = `https://ufl.sona-systems.com/webstudy_credit.aspx?experiment_id=145&credit_token=9419af749b2c4e79b6a43900be9aec70&survey_code=${survey_code}`
 
         // Define Experiment Trials
         let WelcomeTrial = {
@@ -23,7 +24,7 @@ function ExampleExperiment(jsSheetHandle, jsPsychHandle, SONAHandle) {
         jsPsychHandle.init({
             timeline: [WelcomeTrial, FinalTrial],
             on_trial_finish: session.insert,
-            on_finish: SONAHandle.redirectToSONA
+            on_finish: window.top.location.href = SONA_URL
         })
     }
 }

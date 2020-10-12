97a7d8e2f2fc81ee712dc70cc34d770b98effa68
Grey Johnson <gjohnson@osisoft.com>
2020-10-12 01:24:50 -0400

    made smart sheet mechanism to organize columns

--- jsSheet/jsSheet.js
+++ jsSheet/jsSheet.js
@@ -3,21 +3,10 @@ const jsSheet = {
     google.script.run.withFailureHandler(onFailure).withSuccessHandler(CreateLocalSession).GetSessionID()
 
     function CreateLocalSession(id) {
-      let keyLookup = new Map()
       onSuccess({
         id: id,
         insert: function(data) {
-          let keys = Object.keys(data)
-          for (const key of keys) {
-              keyLookup.set(key, true)
-          }
-
-          let paddedData = [id]
-          for (const key of keyLookup.keys()) {
-              paddedData.push(data.hasOwnProperty(key) ? data[key] : '')
-          }
-
-          google.script.run.withFailureHandler(onFailure).Insert(id, paddedData)
+          google.script.run.withFailureHandler(onFailure).Insert(id, data)
         }
       })
     }

89ab741d53df5d53541b40b75ea2a417a657bbb7
GreyJohnsonGit <grey.johnson@ufl.edu>
2020-12-01 10:31:44 -0500

    added jsheet debug mode

--- jsSheet/jsSheet.js
+++ jsSheet/jsSheet.js
@@ -1,13 +1,22 @@
 const jsSheet = {
   CreateSession: function(onSuccess, onFailure = console.error) {
-    google.script.run.withFailureHandler(onFailure).withSuccessHandler(CreateLocalSession).GetSessionID()
+    if (typeof(google) != "undefined") {
+      google.script.run.withFailureHandler(onFailure).withSuccessHandler(CreateLocalSession).GetSessionID()
 
-    function CreateLocalSession(id) {
+      function CreateLocalSession(id) {
+        onSuccess({
+          id: id,
+          insert: function(data) {
+            google.script.run.withFailureHandler(onFailure).Insert(id, data)
+          }
+        })
+      }
+    }
+    else {
+      console.error("\"google\" not defined: running in debug mode")
       onSuccess({
-        id: id,
-        insert: function(data) {
-          google.script.run.withFailureHandler(onFailure).Insert(id, data)
-        }
+        id: -1,
+        insert: function() {}
       })
     }
   }
